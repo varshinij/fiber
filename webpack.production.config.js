79cb70e8cd8b0d7a0251f55200d61e6e870178c5
varshinij <varshini.jagannath@colorado.edu>
2016-04-01 16:53:11 -0600

    annotation done

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,57 +1,62 @@
-var path = require('path');
-var webpack = require('webpack');
+/*The production setting builds the result folder  */
 
-module.exports = {
-  entry: {
-    app : [
+var path = require('path'); // Loa1d module path
+var webpack = require('webpack'); // Load module webpack
+
+
+module.exports = {// export the configuration object
+
+  entry: { //The entry point for the bundle
+    app : [ //list of entry points
       './lib/index.js'],
   },
-  output: {
-    path: path.join(__dirname, './public/js/'),
-    filename: `app.js`,
-    publicPath: '/js/'
+  output: { //output of the compilation
+    path: path.join(__dirname, './public/js/'), //determines the location on disk the files are written to
+    filename: `app.js`, //Specifies the name of each output file on disk
+    publicPath: '/js/' //public URL address of the output files when referenced in a browser
   },
   plugins: [
-    new webpack.DefinePlugin({
+    new webpack.DefinePlugin({ //Define free variables
         'process.env': {
           'NODE_ENV': JSON.stringify('production')
         }
     }),
-    new webpack.optimize.UglifyJsPlugin({
+    new webpack.optimize.UglifyJsPlugin({ //Minimize all JavaScript output of chunks. Loaders are switched into minimizing mode
     }),
   ],
   node: {
     fs: "empty"
   },
-  resolve: {
+  resolve: { //resolving of modules
     alias: {
-      'react': path.join(__dirname, 'node_modules', 'react')
+      'react': path.join(__dirname, 'node_modules', 'react') //replace with these files
     },
-    extensions: ['', '.js']
+    extensions: ['', '.js'] //An array of extensions that should be used to resolve modules
   },
   resolveLoader: {
-    'fallback': path.join(__dirname, 'node_modules')
+    'fallback': path.join(__dirname, 'node_modules') //A directory in which webpack should look for modules that weren’t found in
   },
   module: {
-    loaders: [
+    loaders: [ //loaders required
     {
-      test: /\.js$/,
-      loaders: ['react-hot', 'babel'],
-      exclude: /node_modules/,
-      include: [path.join(__dirname,'./lib')]
+    {
+      test: /\.js$/, // "test" is used to match the file extension js
+      loaders: ['react-hot', 'babel'], // the "loader" react-hot and babel
+      exclude: /node_modules/, // used to exclude exceptions node/modules/
+      include: [path.join(__dirname,'./lib')] // "include" is used to match the directory lib
     },
     {
-      test: /\.xml$/,
-      loader: "raw"
+      test: /\.xml$/, // "test" is used to match the file extension xml
+      loader: "raw" // the "loader" raw
     },
     {
-      test: /\.json$/,
-      loaders: ['json-loader']
+      test: /\.json$/, //"test" is used to match the file extension json
+      loaders: ['json-loader'] // the loader json-loader
     },
     {
-      test: /\.css?$/,
-      loaders: ['style', 'raw'],
-      include: __dirname
-    }]
+      test: /\.css?$/, // "test" is used to match the file extension css
+      loaders: ['style', 'raw'], // the loader style and raw
+      include: __dirname //"include " to match the directory
+          }]
   }
 };
